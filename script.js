8986b3a4cee9be9295637e6bbb7355715de2a961
utkarsh1121 <utkarshtiwari.1121@gmail.com>
2026-05-14 19:32:25 +0530

    changes

---
+++ script.js
@@ -0,0 +1,13 @@
+// const submitBtn = document.querySelector('.search-btn');
+
+// submitBtn.addEventListener('click', async (e) => {
+//     e.preventDefault();
+//     fetch('index.php', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         }
+//     })
+// const data = await response.json();
+// console.log(data);
+// });
